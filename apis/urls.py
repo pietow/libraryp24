9b74799fa04c02d21f2a02e6fd1a08d93ac229d5
piet <pprotassow@posteo.de>
2024-09-18 12:02:28 +0200

    Rest api

---
+++ apis/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import BookAPIViews
+
+urlpatterns = [
+    path("", BookAPIViews.as_view(), name="book_list"),
+]
---
+++ apis/views.py
@@ -0,0 +1,8 @@
+from rest_framework.generics import ListAPIView
+from books.models import Book
+from .serializers import BookSerializer
+
+
+class BookAPIViews(ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
